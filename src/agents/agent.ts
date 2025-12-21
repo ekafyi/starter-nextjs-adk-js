c6c3caeee8e9ba995b3e478c81127fe187532a3c
Eka <6597211+ekafyi@users.noreply.github.com>
2025-12-22 01:32:22 +0700

    implement countries agent with tools

--- src/agents/agent.ts
+++ src/agents/agent.ts
@@ -1,14 +1,58 @@
-import { LlmAgent } from "@google/adk";
+import { FunctionTool, LlmAgent } from "@google/adk";
+import { z } from "zod";
 
-const ROOT_AGENT_NAME = "sample_agent";
+const ROOT_AGENT_NAME = "countries_agent";
 const ROOT_AGENT_MODEL = "gemini-2.5-flash";
-const ROOT_AGENT_DESCRIPTION = "Agent that only serves to confirm ADK library is running.";
-const ROOT_AGENT_INSTRUCTION = "You reply with short acknowledgment that you are indeed a running agent.";
+const ROOT_AGENT_DESCRIPTION =
+	"Agent to answer questions about the capital or flag of a country.";
+const ROOT_AGENT_INSTRUCTION = "You are a simple geography-themed assistant. You reply to user's query about the capital or flag of a country. If user asks about anything else, refuse to answer and say why.";
+
+/**
+ * Tool (1) to retrieve the capital city for a given country.
+ */
+const getCapital = new FunctionTool({
+	name: "get_country_capital",
+	description: "Retrieves the capital city for a given country.",
+	parameters: z.object({
+		country: z
+			.string()
+			.describe(
+				"The name of the country for which to retrieve the capital city.",
+			),
+	}),
+	execute: async ({ country }) => {
+		return {
+			status: "success",
+			result: `TODO return capital of ${country}`,
+		};
+	},
+});
+
+/**
+ * Tool (2) to retrieve the flag emoji for a given country.
+ */
+const getFlag = new FunctionTool({
+	name: "get_country_flag",
+	description: "Retrieves the flag emoji for a given country.",
+	parameters: z.object({
+		country: z
+			.string()
+			.describe(
+				"The name of the country for which to retrieve the flag emoji.",
+			),
+	}),
+	execute: async ({ country }) => {
+		return {
+			status: "success",
+			result: `TODO return flag of ${country}`,
+		};
+	},
+});
 
 export const rootAgent = new LlmAgent({
 	name: ROOT_AGENT_NAME,
 	model: ROOT_AGENT_MODEL,
 	description: ROOT_AGENT_DESCRIPTION,
 	instruction: ROOT_AGENT_INSTRUCTION,
-	tools: [],
+	tools: [getCapital, getFlag]
 });
